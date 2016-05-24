4c61ea4ff146f9e1d92d075a918b763bb8a67eb0
unknown <>
2016-05-24 23:15:13 +0800

    add

---
+++ hw6/parity.py
@@ -0,0 +1,9 @@
+import random
+
+number = random.randint(0, 100)
+
+# Finish the program by edit code below.
+if number % 2 ==0:
+    print '%d is even.' % number
+else:
+    print '%d is odd.' % number
