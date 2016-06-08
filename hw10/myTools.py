a3ca152fc9a9d6b3f4918aa7261855ef159357e7
unknown <>
2016-06-08 22:31:47 +0800

    add

---
+++ hw10/myTools.py
@@ -0,0 +1,13 @@
+def sum(lst):
+    # Your code here. (Also edit the return statement.)
+    a=0
+    for x in lst:
+    	a=a+x
+
+
+    return a
+
+def avg(lst):
+    # Your code here. (Also edit the return statement.)
+
+    return sum(lst)/len(lst)
---
+++ hw12/aboutToBreak.py
@@ -0,0 +1,30 @@
+# Pressure rising every day by a fixed portion. Finish the program 
+# using break statement, see if you can survive the year.
+
+import random
+
+pressure = 1.0
+pressureRate = 1 + random.randint(7, 17) / 1000.0
+pressureLimit = 100.0   # how much pressure you can endure
+print 'pressureRate = %f' % pressureRate
+day = 0
+
+broken = False
+
+while day < 365:
+    pressure *= pressureRate
+    print 'Pressure rising: %.3f' % pressure
+
+    # Your code here. 
+    if pressure<pressureLimit:
+        day += 1 
+    else:
+    	broken=True
+    	break
+    	
+    
+if broken:
+
+    print 'Too much pressure, you break on day %d' % day
+else:
+    print 'Luckily, you survived the year.'
